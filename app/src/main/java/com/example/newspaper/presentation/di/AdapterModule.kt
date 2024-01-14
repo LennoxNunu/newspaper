711d6bbfcf2a8ad9f3e6368ffcf4e9af87c6c0fc
LennoxNunu <lennoxnunu@gmail.com>
2024-01-14 13:57:22 +0200

    more dependency injection

---
+++ app/src/main/java/com/example/newspaper/presentation/di/AdapterModule.kt
@@ -0,0 +1,18 @@
+package com.example.newspaper.presentation.di
+
+import com.example.newspaper.presentation.adapter.NewsAdapter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AdapterModule {
+    @Singleton
+    @Provides
+    fun providesNewsAdapter():NewsAdapter{
+        return NewsAdapter()
+    }
+}
